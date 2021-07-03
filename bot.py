170c1943389012ec94bca55ced65584455a5e431
HEIMAN PICTURES <78695802+HeimanPictures@users.noreply.github.com>
2021-07-03 14:37:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,37 @@
-import pyrogram
-from config import Config 
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+from config import Config as C
 
-plugins = dict(
-    root="plugins"
-)
-app = pyrogram.Client(
-    "Maintain Bot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    plugins=plugins
-)
-app.run()
+PORT = int(C.PORT)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    update.message.reply_text(C.START_TEXT)
+
+def all(update, context):
+    update.message.reply_text(C.MAINTAINANCE_TEXT)
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    updater = Updater(C.BOT_TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.all, all))
+    dp.add_error_handler(error)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=C.BOT_TOKEN)
+    updater.bot.setWebhook('https://{C.APP_NAME}.herokuapp.com/' + C.BOT_TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
